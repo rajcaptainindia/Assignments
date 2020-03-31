59dae7acf6f774c728b0c898d3da5738d3c9469a
rajcaptainindia <rajcsecr@gmail.com>
2020-03-31 16:50:35 +0530

    Create Assignment25.py

---
+++ Assignment25.py
@@ -0,0 +1,13 @@
+#PF-Tryout
+
+#debug the below code
+counter1=0
+counter2=5
+while(counter1 < 5):
+  star=""
+  while(counter2>counter1):
+     star="*"*counter2
+     print(star)
+     counter2-=1
+  
+  counter1+=1
